677e66d1f4136583d5914daf793130391e75cc1e
keita <keita.timeleap@gmail.com>
2020-07-05 15:47:43 +0900

    db migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # get 'home/top'
   root "home#top"
+
+  devise_for :users
+  
+  devise_scope :user do
   get "login" => "devise/sessions#new"
   get "signup" => "devise/registrations#new"
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
