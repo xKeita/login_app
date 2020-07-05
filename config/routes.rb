69d0ca3f79a6b8ca85391e71c535ade82dbde0f3
keita <keita.timeleap@gmail.com>
2020-07-05 16:54:02 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root "home#top"
 
   devise_for :users
-  
+
   devise_scope :user do
   get "login" => "devise/sessions#new"
   get "signup" => "devise/registrations#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
