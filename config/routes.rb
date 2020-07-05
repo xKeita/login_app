25bd2d628f1d97671439036e07de3d2a5d61349e
keita <keita.timeleap@gmail.com>
2020-07-05 13:43:39 +0900

    fix root routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/top'
-  
+  # get 'home/top'
+  root "home#top"
+  get "login" => "users/sign_in"
+  get "signup" => "users/sign_up"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
